872c99b3397091330f9aaff93fceddfe4af2fc68
CBeuster <kontakt@christian-beuster.de>
2025-05-04 17:30:44 +0200

    Update english - german.bat

--- english - german.bat
+++ english - german.bat
@@ -1,27 +1,2 @@
 @echo off
-setlocal enabledelayedexpansion
-
-for /r %%f in (*_l_english.yml) do (
-    echo Bearbeite %%f
-    set "file=%%f"
-    set "tmpfile=%%f.tmp"
-
-    rem --- Dateiinhalt ändern ---
-    (for /f "usebackq delims=" %%l in ("%%f") do (
-        set "line=%%l"
-        set "line=!line:english=german!"
-        echo(!line!
-    )) > "!tmpfile!"
-
-    rem --- Alte Datei ersetzen ---
-    move /y "!tmpfile!" "%%f" > nul
-
-    rem --- Dateinamen anpassen ---
-    set "filepath=%%~dpf"  & rem Pfad
-    set "filename=%%~nxf" & rem Dateiname mit Erweiterung
-    set "newname=!filename:_l_english.yml=_l_german.yml!"
-    ren "%%f" "!newname!"
-    echo Umbenannt: !filename! → !newname!
-)
-
-echo Alle Dateien verarbeitet.
+powershell -ExecutionPolicy Bypass -File "%~dp0konvertiere_localisation.ps1"
